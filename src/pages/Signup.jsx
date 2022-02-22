2accfffc8c8e4745c8889016d9814dd358a3c2d4
VSCODER <vscodr.brad@gmail.com>
2022-02-22 17:32:20 +0800

    Added Auth Peices

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {setDoc, doc, serverTimestamp } from "firebase/firestore"; 
+import {db} from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -19,6 +22,35 @@ function Signup() {
             [e.target.id] : e.target.value,
         }))
      }
+    const onSubmit = async(e) =>{
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword
+            (
+                auth, 
+                email, 
+                password
+            )
+
+            const user = userCredential.user
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            const formDataCopy = {...formData}
+            delete formDataCopy.password            
+            formDataCopy.timestamp = serverTimestamp()
+
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+            navigate('/')
+        } catch (error){
+console.log(error);
+        }
+    }
     const { name, email, password } = formData
     const navigate = useNavigate()
     return (
@@ -28,7 +60,7 @@ function Signup() {
                     <p className="pageHeader">Sign Up Here</p>
                 </header>
                 <main>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <input
                             type="text"
                             className="nameInput"
